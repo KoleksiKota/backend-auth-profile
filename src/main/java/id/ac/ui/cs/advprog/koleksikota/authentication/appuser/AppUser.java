ea07eabcae7db41ae8bca1273be042ca4ec4d778
rigifdaga <rfdauliaa@gmail.com>
2024-05-08 17:24:43 +0700

    [REFACTOR] Change import in AppUser

--- src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/appuser/AppUser.java
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/appuser/AppUser.java
@@ -5,11 +5,10 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 
 import java.util.Collection;
 import java.util.Collections;
