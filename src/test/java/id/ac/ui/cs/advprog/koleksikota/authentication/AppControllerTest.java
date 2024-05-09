91a95aab4770b025cfda245bb05b239221e56d86
rigifdaga <rfdauliaa@gmail.com>
2024-05-09 09:55:33 +0700

    [RED] Add failing test for AppController

---
+++ src/test/java/id/ac/ui/cs/advprog/koleksikota/authentication/AppControllerTest.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.advprog.koleksikota.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(AppController.class)
+public class AppControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserRepository userRepo;
+
+    @Test
+    public void testViewHomePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
