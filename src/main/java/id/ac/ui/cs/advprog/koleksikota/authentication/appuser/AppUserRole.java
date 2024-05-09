e200b213237a4ff3f280af6202ad9023e749920b
rigifdaga <rfdauliaa@gmail.com>
2024-05-09 09:20:21 +0700

    [RED] Add failing test for UserRepository

--- src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/appuser/AppUserRole.java
+++
@@ -1,6 +0,0 @@
-package id.ac.ui.cs.advprog.koleksikota.authentication.appuser;
-
-public enum AppUserRole {
-    USER,
-    ADMIN
-}
--- src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/email/EmailSender.java
+++
@@ -1,5 +0,0 @@
-package id.ac.ui.cs.advprog.koleksikota.authentication.email;
-
-public interface EmailSender {
-    void send(String to, String email);
-}
