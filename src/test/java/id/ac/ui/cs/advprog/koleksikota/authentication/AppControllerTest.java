27c7d63b7f93c33b7a753ead615396c6211fa3d4
rigifdaga <rfdauliaa@gmail.com>
2024-05-09 10:51:07 +0700

    [RED] Add failing test for registration form page

--- src/test/java/id/ac/ui/cs/advprog/koleksikota/authentication/AppControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/koleksikota/authentication/AppControllerTest.java
@@ -10,8 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AppController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -28,5 +27,13 @@ public class AppControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("homepage"));
+    }
+
+    @Test
+    public void testShowRegistrationForm() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/register"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("signup_form"))
+                .andExpect(model().attributeExists("user"));
     }
 }
