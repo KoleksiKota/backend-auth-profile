6e07fdc66d919364cff8a36ebfd27057300a7ce0
rigifdaga <rfdauliaa@gmail.com>
2024-05-09 11:18:46 +0700

    [GREEN] Implement Register page

--- src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/controller/AppController.java
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/controller/AppController.java
@@ -1,9 +1,13 @@
 package id.ac.ui.cs.advprog.koleksikota.authentication.controller;
 
+import id.ac.ui.cs.advprog.koleksikota.authentication.model.User;
 import id.ac.ui.cs.advprog.koleksikota.authentication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
@@ -14,5 +18,23 @@ public class AppController {
     @GetMapping("")
     public String viewHomePage() {
         return "homepage";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepo.save(user);
+
+        return "register_success";
     }
 }
