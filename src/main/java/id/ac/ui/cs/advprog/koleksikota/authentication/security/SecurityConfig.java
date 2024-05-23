191e846e35c35a7b50410e62f20deaad528a05b8
rigifdaga <rfdauliaa@gmail.com>
2024-05-23 09:00:16 +0700

    [REFACTOR] Modify CustomUserDetailService and SecurityConfig

--- src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/security/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package id.ac.ui.cs.advprog.koleksikota.authentication.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,12 +23,19 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private CustomUserDetailService userDetailService;
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailService userDetailService) {
+        this.userDetailService = userDetailService;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .requestMatchers(HttpMethod.GET).authenticated()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
@@ -45,6 +57,17 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(admin,user);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
