1513943a677241d354176320c0764a0e597df795
rigifdaga <rfdauliaa@gmail.com>
2024-05-23 08:37:43 +0700

    [REFACTOR] Modify models

---
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/models/Role.java
@@ -0,0 +1,19 @@
+package id.ac.ui.cs.advprog.koleksikota.authentication.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Setter
+@Getter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+
+    private String name;
+
+}
