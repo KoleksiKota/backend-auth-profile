d686817145d73a26ec0cdca3fccad16963d7eac5
rigifdaga <rfdauliaa@gmail.com>
2024-05-25 10:37:00 +0700

    Modify user profile controller and service

--- src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/controllers/UserProfileController.java
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/controllers/UserProfileController.java
@@ -1,8 +1,6 @@
 package id.ac.ui.cs.advprog.koleksikota.authentication.controllers;
 
 import id.ac.ui.cs.advprog.koleksikota.authentication.dto.UserProfileDto;
-import id.ac.ui.cs.advprog.koleksikota.authentication.models.UserProfile;
-import id.ac.ui.cs.advprog.koleksikota.authentication.repository.UserRepository;
 import id.ac.ui.cs.advprog.koleksikota.authentication.service.UserProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,27 +11,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/profile")
 public class UserProfileController {
     private UserProfileService userProfileService;
-    private UserRepository userRepository;
 
     @Autowired
-    public UserProfileController(UserProfileService userProfileService, UserRepository userRepository){
+    public UserProfileController(UserProfileService userProfileService){
         this.userProfileService = userProfileService;
-        this.userRepository = userRepository;
-    }
-
-    @PostMapping("/{username}")
-    public ResponseEntity<String> createProfile(@PathVariable String username, @RequestBody UserProfileDto userProfileDto){
-        return userRepository.findByUsername(username).map(user -> {
-            userProfileService.createUserProfile(user, userProfileDto);
-            return new ResponseEntity<>("User profile created successfully", HttpStatus.OK);
-        }).orElse(new ResponseEntity<>("User not found", HttpStatus.BAD_REQUEST));
     }
 
     @GetMapping("/{username}")
-    public ResponseEntity<UserProfile> getProfile(@PathVariable String username){
-        UserProfile userProfile = userProfileService.getUserProfileByUsername(username);
-        if (userProfile != null) {
-            return new ResponseEntity<>(userProfile, HttpStatus.OK);
+    public ResponseEntity<UserProfileDto> getProfile(@PathVariable String username) {
+        UserProfileDto userProfileDto = userProfileService.getUserProfileByUsername(username);
+        if (userProfileDto != null) {
+            return new ResponseEntity<>(userProfileDto, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
--- src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/service/UserProfileService.java
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/service/UserProfileService.java
@@ -2,7 +2,6 @@ package id.ac.ui.cs.advprog.koleksikota.authentication.service;
 
 import id.ac.ui.cs.advprog.koleksikota.authentication.dto.UserProfileDto;
 import id.ac.ui.cs.advprog.koleksikota.authentication.models.UserProfile;
-import id.ac.ui.cs.advprog.koleksikota.authentication.models.UserEntity;
 import id.ac.ui.cs.advprog.koleksikota.authentication.repository.UserProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,17 +15,20 @@ public class UserProfileService {
         this.userProfileRepository = userProfileRepository;
     }
 
-    public UserProfile createUserProfile(UserEntity user, UserProfileDto userProfileDto) {
-        UserProfile userProfile = UserProfile.builder()
-                .fullName(userProfileDto.getFullName())
-                .phoneNumber(userProfileDto.getPhoneNumber())
-                .address(userProfileDto.getAddress())
-                .user(user)
-                .build();
-        return userProfileRepository.save(userProfile);
+    public UserProfileDto getUserProfileByUsername(String username) {
+        UserProfile userProfile = userProfileRepository.findByUser_Username(username).orElse(null);
+        if (userProfile != null) {
+            return convertToDto(userProfile);
+        } else {
+            return null;
+        }
     }
 
-    public UserProfile getUserProfileByUsername(String username) {
-        return userProfileRepository.findByUser_Username(username).orElse(null);
+    private UserProfileDto convertToDto(UserProfile userProfile) {
+        UserProfileDto dto = new UserProfileDto();
+        dto.setFullName(userProfile.getFullName());
+        dto.setPhoneNumber(userProfile.getPhoneNumber());
+        dto.setAddress(userProfile.getAddress());
+        return dto;
     }
 }
