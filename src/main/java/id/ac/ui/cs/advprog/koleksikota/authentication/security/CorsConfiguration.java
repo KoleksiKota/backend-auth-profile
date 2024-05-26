f1c102b45c7ed927f6f17de6974e826d8fe2b16b
rigifdaga <rfdauliaa@gmail.com>
2024-05-26 18:22:53 +0700

    [ADD] Add deployed frontend to CorsConfiguration

--- src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/security/CorsConfiguration.java
+++ src/main/java/id/ac/ui/cs/advprog/koleksikota/authentication/security/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "https://tk-adpro-b11-frontend.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
